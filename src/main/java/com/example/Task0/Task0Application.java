39b30b5d2298e2775dd3a71f3f21b2c21536f13c
Sergey <chigagog@gmail.com>
2021-12-09 18:47:42 +0300

    initial commit

---
+++ src/main/java/com/example/Task0/Task0Application.java
@@ -0,0 +1,13 @@
+package com.example.Task0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task0Application.class, args);
+	}
+
+}
